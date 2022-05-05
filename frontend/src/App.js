706adf7b329d6ba766ef22cd23b25278611970c1
Iamkeetarp <prateekshukla978@gmail.com>
2022-05-06 00:20:45 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { Routes } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path = "/product/:slug" element = {<ProductScreen/>} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
