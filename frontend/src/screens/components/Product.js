018d5ddd5a5d5b934fa02a4212d4e86a5ed215a4
Iamkeetarp <prateekshukla978@gmail.com>
2022-05-14 01:45:09 +0530

    Sign In screen Completed

--- frontend/src/screens/components/Product.js
+++ frontend/src/screens/components/Product.js
@@ -1,13 +1,14 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 import axios from "axios";
 import { useContext } from "react";
-import { Button } from "react-bootstrap";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { Store } from "../../Store";
-import Rating from "./Rating";
 
 function Product(props) {
   const { product } = props;
+
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
@@ -16,7 +17,7 @@ function Product(props) {
   const addToCartHandler = async (item) => {
     const existItem = cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`api/products/${item._id}`);
+    const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert("Sorry. Product is out of stock");
       return;
@@ -26,23 +27,25 @@ function Product(props) {
       payload: { ...item, quantity },
     });
   };
+
   return (
-    <Card className="product">
+    <Card>
       <Link to={`/product/${product.slug}`}>
-        <img
-          src={product.image}
-          className="card-img-top"
-          alt={product.name}
-          width="400px"
-        />
+        <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
       <Card.Body>
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>{product.price}₹</Card.Text>
-        <Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+        <Card.Text>${product.price}</Card.Text>
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
