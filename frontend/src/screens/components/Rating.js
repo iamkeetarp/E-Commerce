51198a55bd858e55e3ee8a8ffa9b7f531c345a57
Iamkeetarp <prateekshukla978@gmail.com>
2022-05-20 18:52:06 +0530

    Implemented search and filter

--- frontend/src/screens/components/Rating.js
+++ frontend/src/screens/components/Rating.js
@@ -1,5 +1,5 @@
 function Rating(props) {
-  const { rating, numReviews } = props;
+  const { rating, numReviews, caption } = props;
   return (
     <div className="rating">
       <span>
@@ -61,10 +61,11 @@ function Rating(props) {
           }
         />
       </span>
-
-      <span>
-          {numReviews}
-      </span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{" " + numReviews + "reviews"}</span>
+      )}
     </div>
   );
 }
